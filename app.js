f1ef98c291e628e6bc0e838b41a3dc48c21f363a
Steven Zielke <steven.zielke>
2017-04-22 16:51:50 +0200

    huau image

--- app.js
+++ app.js
@@ -18,25 +18,36 @@ var connector = new builder.ChatConnector
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
+// Anytime the major version is incremented any existing conversations will be restarted.
+bot.use(builder.Middleware.dialogVersion({ version: 1.0, resetCommand: /^reset/i }));
+
+//=========================================================
+// Bots Global Actions
+//=========================================================
+
+bot.endConversationAction('goodbye', 'Goodbye :)', { matches: /^goodbye/i });
+bot.beginDialogAction('help', '/help', { matches: /^help/i });
+
 // Create bot dialogs
 bot.dialog('/', 
 
 function (session) {
     var card = new builder.HeroCard(session)
-            .title("Microsoft Bot Framework")
-            .text("Your bots - wherever your users are talking.")
+            .title("HU / AU Planer")
+            .text("ich helfe Ihnen dabei einen Termin zur HU/AU zu vereinbaren.")
             .images([
-                 builder.CardImage.create(session, "http://docs.botframework.com/images/demo_bot_image.png")
+                 builder.CardImage.create(session, "https://mobilapp-chatbot.azurewebsites.net/images/huau.jpg")
             ]);
         var msg = new builder.Message(session).attachments([card]);
-session.send(msg);
-    session.send("Hallo2.");
+    session.send(msg);
+    //session.send(session.userData.name);
+    session.send("Wie kann ich Ihnen helfen? Mit einer HU, oder einer AU?");
     session.beginDialog('/menu');
 });
 
 bot.dialog('/menu', [
     function (session) {
-        builder.Prompts.choice(session, "What demo would you like to run?", "prompts|picture|cards|list|carousel|receipt|actions|(quit)");
+        builder.Prompts.choice(session, "Wie kann ich IHnen helfen? Mit einer HU, oder einer AU?", "HU|AU|(abbrechen)");
     },
     function (session, results) {
         if (results.response && results.response.entity != '(quit)') {
