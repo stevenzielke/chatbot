6814be558a0e40a7912f3be3140d259599f24af0
Steven Zielke <steven.zielke>
2017-04-23 07:10:20 +0200

    test

--- app.js
+++ app.js
@@ -9,8 +9,64 @@ server.listen(process.env.PORT || 3000, function()
    console.log('%s listening to %s', server.name, server.url); 
 });
 
-//var appId = process.env.MY_APP_ID,
-//var appPassword = process.env.MY_APP_PASSWORD;
+var tuevStations = {};
+tuevStations["stations"] = [];
+function createTUEVStationsArr () {
+    var data = {
+        lat: '53.4903476',
+        lng: '10.2026361',
+        address: 'Bergedorfer Straße 74, 21033 Hamburg'
+    };
+    tuevStations["stations"].push(data);
+    data = {
+        lat: '48.44719',
+        lng: '11.13818',
+        address: 'Rudolf-Diesel-Str. 3, 86551 Aichach'
+    };
+    tuevStations["stations"].push(data);
+
+
+}
+createTUEVStationsArr();
+console.log(tuevStations);
+
+function getGeo(ort) {
+    // setup Google Maps
+
+    var googleMapsClient = require('@google/maps').createClient({
+        key: 'AIzaSyBn6M9Sl3K4tRn0QbkqN5QPSdHoNMIvndw'
+    });
+    googleMapsClient.geocode({
+        address: ort
+        }, function(err, response) {
+        if (!err) {
+            for(var i = 0; i < response.json.results.length; i++) {
+                if (response.json.results[i].geometry) {
+                    var lat = response.json.results[i].geometry.location.lat;
+                    var lng = response.json.results[i].geometry.location.lng;
+                    console.log(lat);
+                    console.log(lng);
+                 }
+            }
+    
+            var util = require("util");
+            //console.log(JSON.stringify(response, null, 4))
+        } else {
+            //console.log(err);
+        }
+        }
+    );
+   
+}
+
+
+
+
+
+
+
+
+
 
 // Create chat bot
 var connector = new builder.ChatConnector
@@ -32,6 +88,7 @@ bot.beginDialogAction('help', '/help', { matches: /^help/i });
 bot.dialog('/', 
 
 function (session) {
+
     var card = new builder.HeroCard(session)
             .title("HU / AU Planer")
             .text("ich helfe Ihnen dabei einen Termin zur HU/AU zu vereinbaren.")
@@ -67,7 +124,21 @@ bot.dialog('/huaumenu', [
 
 bot.dialog('/HU', [
 function (session) {
-        session.send("Wo möchten Sie denn die Hauptuntersuchung durchführen? Bitte geben Sie die PLZ oder Adresse ein und ich schlage eine TÜV Station in der Nähe vor.");
+        builder.Prompts.text(session, "Wo möchten Sie denn die Hauptuntersuchung durchführen? Bitte geben Sie die PLZ oder Adresse ein und ich schlage eine TÜV Station in der Nähe vor.");
+        //session.send("Wo möchten Sie denn die Hauptuntersuchung durchführen? Bitte geben Sie die PLZ oder Adresse ein und ich schlage eine TÜV Station in der Nähe vor.");
+    },
+    function (session, results) {
+        var ort = results.response;
+        session.send("Einen Moment. Ich suche nach einer TÜV Station in der Nähe der Adresse: " + ort);
+
+        //if(Number(ort) = 'NaN')
+        //{
+        //    session.send("Einen Moment. Ich suche nach einer TÜV Station in der Nähe der Adresse: " + ort);
+        //} else {
+        //    session.send("Einen Moment. Ich suche nach einer TÜV Station in der Nähe der PLZ: " + ort);
+        //}
+        getGeo(ort);
+       // builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
     }
 ]);
 
